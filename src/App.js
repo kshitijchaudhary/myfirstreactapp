25237a16ba77da29e759d0580dd633119ecb1cbd
LCF2304 <c0920457@mylambton.ca>
2023-12-24 12:23:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import SubmitButton from './components/button.js'
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+      
+//         <h1>Hello Mahan Saksena <br/>
+//           Hello XITOL DON
+//         </h1>
+
+   
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <SubmitButton/>
     </div>
   );
 }
 
-export default App;
---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from "react";
+function SubmitButton() {
+    return (
+      <button>
+        Submit Button
+      </button>
+    );
+  }
+
+  export default SubmitButton;
